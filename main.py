003ef90a903005ec565b12fffc21b4a964e27954
Roshan Mishal <65908970+Roshan-collab@users.noreply.github.com>
2023-03-05 05:21:30 +0000

    dynamic data

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': "Bangluru",
+    'salary':'100000 rs',    
+    },
+    {
+    'id': 2,
+    'title': "Data scientist",
+    'location': "Mumbai",
+    'salary':'150000 rs'    
+    },
+    {  
+    'id': 3,
+    'title': "Data Engineer",
+    'location': "Bangluru",   
+    },
+    {
+    'id': 4,
+    'title': "Backend engineer",
+    'location': "remote",
+    'salary':'105000 rs'    
+    }
+  ]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs = JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run('0.0.0.0',debug = True)
